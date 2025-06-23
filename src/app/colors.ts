ef61814358448e58a266e1d2cbb6f25fa8b032d3
Tom Hanson <iTomHanson@gmail.com>
2025-06-23 22:36:40 +0100

    :sparkles: Initial commit

---
+++ src/app/colors.ts
@@ -0,0 +1,28 @@
+
+export const colors =   [
+  {
+    "name": "Salt Cellar",
+    "twBackground": "red-400",
+    "twForeground": "gray-800",
+    "id": "0da0af6a-8ce6-4dfb-8e90-8fe205420061"
+  },
+  {
+    "name": "Well-Water & Ink",
+    "twBackground": "rose-400",
+    "twForeground": "yellow-50",
+    "id": "89120616-09db-4c30-92be-d0c067bb0c54"
+  },
+  {
+    "name": "Chalkboard Afternoon",
+    "twBackground": "teal-700",
+    "twForeground": "gray-100",
+    "id": "04298a5d-2fcc-4a72-9bb6-7d3da4480851"
+  },
+  {
+    "name": "Milled Oat",
+    "twBackground": "gray-800",
+    "twForeground": "blue-500",
+    "id": "04298a5d-2fcc-4a72-9bb6-7d3da4480852"
+  },
+  
+]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,131 @@
-import Image from "next/image";
+"use client";
+
+import { useRef } from "react";
+
+import { colors } from "./colors";
+
+const TEXT_COLORS = {
+  "gray-100": "text-gray-100",
+  "gray-800": "text-gray-800",
+  "rose-400": "text-rose-400",
+  "amber-400": "text-amber-400",
+  black: "text-black",
+  white: "text-white",
+  "slate-700": "text-slate-700",
+  "indigo-500": "text-indigo-500",
+  "blue-500": "text-blue-500",
+  "red-500": "text-red-500",
+  "green-500": "text-green-500",
+  "lime-400": "text-lime-400",
+  "teal-400": "text-teal-400",
+  "cyan-400": "text-cyan-400",
+  "pink-500": "text-pink-500",
+  "sky-400": "text-sky-400",
+  "yellow-50": "text-yellow-50",
+};
+
+const BG_COLORS = {
+  "gray-100": "bg-gray-100",
+  "gray-800": "bg-gray-800",
+  "rose-400": "bg-rose-400",
+  "amber-400": "bg-amber-400",
+  black: "bg-black",
+  white: "bg-white",
+  "slate-700": "bg-slate-700",
+  "indigo-500": "bg-indigo-500",
+  "blue-500": "bg-blue-500",
+  "red-400": "bg-red-400",
+  "red-500": "bg-red-500",
+  "green-500": "bg-green-500",
+  "lime-400": "bg-lime-400",
+  "teal-400": "bg-teal-400",
+  "teal-700": "bg-teal-700",
+  "cyan-400": "bg-cyan-400",
+  "pink-500": "bg-pink-500",
+  "sky-400": "bg-sky-400",
+  "yellow-50": "bg-yellow-50",
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const scrollTargetRef = useRef<HTMLDivElement | null>(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const handleScroll = () => {
+    scrollTargetRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <div className="items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px]">
+        <div className="bg-white">
+          <div className="mx-auto max-w-7xl py-24 sm:py-32">
+            <h2 className="max-w-2xl text-balance text-4xl tracking-tight text-gray-900 sm:text-5xl font-[family-name:var(--font-lora-serif)] font-black">
+              Tailwind Colors
+            </h2>
+            <h2 className="mt-20 max-w-2xl text-balance text-3xl tracking-tight text-gray-900 sm:text-5xl font-[family-name:var(--font-lora-serif)] font-semibold">
+              Find pleasing and awesome color combinations, complete with
+              Tailwind class names
+            </h2>
+            <div className="mt-20 flex items-center gap-x-6">
+              <a
+                href="#"
+                onClick={handleScroll}
+                className="flex flex-row rounded-full bg-indigo-600 px-5 py-2.5 text-md font-[family-name:var(--font-figtree-sans)] font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 items-center"
+              >
+                Take a look
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2.5}
+                  stroke="currentColor"
+                  className="size-5 ml-2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div
+          ref={scrollTargetRef}
+          className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"
+        >
+          {colors.map((item) => (
+            <div key={item.id} className="group relative">
+              <div
+                className={`mt-4 flex justify-between bg-${item.twBackground} aspect-square h-80 w-80 rounded-3xl flex items-center justify-center`}
+              >
+                <div>
+                  <h3
+                    className={`text-2xl text-${item.twForeground} font-[family-name:var(--font-figtree-sans)] font-bold text-center px-10 break-words leading-snug`}
+                  >
+                    {item.name.split(" ").length > 1 ? (
+                      <>
+                        {item.name.split(" ")[0]} <br />{" "}
+                        {item.name.split(" ").slice(1).join(" ")}
+                      </>
+                    ) : (
+                      item.name
+                    )}
+                  </h3>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center mt-80">
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4 font-[family-name:var(--font-figtree-sans)]"
+          href="https://tmhn.io"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+          &copy; {new Date().getFullYear()} - Tom Hanson
         </a>
       </footer>
     </div>
